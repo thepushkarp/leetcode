aaf0284d3efa5ffb187518086e91c9b2c9a00b49
Pushkar Patel <thepushkarp@gmail.com>
2020-05-20 19:18:50 +0530

    Add May Day 20

---
+++ MayChallenge/Week3/20:KthSmalletsElementinaBST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        stack<TreeNode*> s;
+        while (true) {
+            while (root != nullptr) {
+                s.push(root);
+                root = root -> left;
+            }
+            root = s.top();
+            s.pop();
+            if (--k == 0) return root -> val;
+            root = root -> right;
+        }
+    }
+};
