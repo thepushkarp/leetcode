cf57fb08e0eaaa2db7c88f99d535f72a98b823e5
Pushkar Patel <thepushkarp@gmail.com>
2020-06-11 19:04:12 +0530

    Add June Day 11

---
+++ JuneChallenge/Week2/11:SortColors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int lo = 0, mid = 0; 
+        int hi = nums.size() - 1; 
+        while (mid <= hi) { 
+            if (nums[mid] == 0)
+                swap(nums[lo++], nums[mid++]); 
+            else if (nums[mid] == 1)
+                mid++;
+            else
+                swap(nums[mid], nums[hi--]); 
+        }
+    }
+};
+
