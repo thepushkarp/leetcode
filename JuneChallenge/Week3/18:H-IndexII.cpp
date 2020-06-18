7631ccf06a0a7f68a5b68154b9149f03eabc501a
Pushkar Patel <thepushkarp@gmail.com>
2020-06-18 22:28:35 +0530

    Better solution of June Day 18

--- JuneChallenge/Week3/18:H-IndexII.cpp
+++ JuneChallenge/Week3/18:H-IndexII.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        if (n == 0) return 0;
-        int lo = 0, mid = 0, ans = -1;
-        int hi = citations.size() - 1;
-        while (lo + 1 < hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (citations[mid] == n - mid) {
-                return n - mid;
-            } else if (citations[mid] < n - mid) {
-                lo = mid;
-            } else {
-                hi = mid;
+        if (!n) return 0;
+        int lo = 0, mid = 0, ans = 0;
+        int hi = n - 1;
+        while(lo <= hi){
+            int mid  = lo + (hi - lo) / 2;
+            if(citations[mid] >= n - mid){
+                ans = n - mid;
+                hi = mid - 1;
+            }
+            else{
+                lo  = mid + 1;
             }
         }
-        if (citations[lo] >= n - lo) {
-            return n - lo;
-        }
-         
-        if (citations[hi] >= n - hi) {
-            return n - hi;
-        }
-        return 0;
+        return ans;
     }
 };
 
